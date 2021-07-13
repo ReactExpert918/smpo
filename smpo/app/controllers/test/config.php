fc94bf3277708f5500b5cb19b1d4b50dd28a0850
ReactExpert918 <harrypotter0918@outlook.com>
2021-07-13 15:12:28 +0300

    update the issues

---
+++ smpo/app/controllers/test/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'siteName' => 'https://kareem.tarana.sa/test/',  //Your website name
+    'username' => 'admin', //Your admin username
+    'password' => 'admin', //Your admin password
+    'adsensPublishKey' => '7178474122577413', //Adsens Publish key
+    'facebookAppID' => '314235422399273',//facebook Application ID    
+    'cardsMailFormat' => 'both', //send card by mail as (attachment | image | both) **** WARNING: (image or both) may servers detect the mail as spam   
+    'senderMail' => 'no-replay@kareem.tarana.sa', //The Sender E-mail address
+    'dir' => '__bgs__', //Orginal Images directory  
+);
